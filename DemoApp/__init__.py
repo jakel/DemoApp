0e3a8978406f9a0691d51247a90072ade3b7d0d9
Dan Jakel <dan.jakel@gmail.com>
2016-06-20 20:17:17 -0700

    adding uwsgi.ini config and fixing app imports

--- DemoApp/__init__.py
+++ DemoApp/__init__.py
@@ -25,15 +25,16 @@ else:
     # set throwaway key for easy debugging
     app.secret_key = '\x91/\x0bu \x88W\rN\x01\xdb\x0f\x1dj\\\xe5\xa7\x94\xe0U\xa7b\x87e'
 
+
+#import routes from controllers
+from DemoApp.main import index
+import DemoApp.users
+import DemoApp.fleets
+import DemoApp.ships
+import DemoApp.map
+
 def main(options, env='dev'):
     init_db(options.debug)
-
-    #import routes from controllers
-    from DemoApp.main import index
-    import DemoApp.users
-    import DemoApp.fleets
-    import DemoApp.ships
-    import DemoApp.map
 
     # start the app
     app.run(host='0.0.0.0', debug=options.debug)
